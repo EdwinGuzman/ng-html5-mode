ec06554c59b98e92212d3390065345a74e411dee
Edwin Guzman <edwin.gzmn@gmail.com>
2014-05-20 17:24:01 -0400

    Commit of everything working with modrewrite and html5 mode

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('testAngularYoApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
